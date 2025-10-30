da19a4522f60f7743271d118f598ddabc23ec25c
hinokamikagura <kairos.stroud@gmail.com>
2025-10-30 15:53:51 -0400

    Feat: add show and update handler

---
+++ handler/request.go
@@ -0,0 +1,63 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+
+type CreatOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreatOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Link == "" && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty")
+	}
+	if r.Role == "" {
+		errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		errParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		errParamIsRequired("location", "string")
+	}
+	if r.Remote == nil {
+		errParamIsRequired("remote", "bool")
+	}
+	if r.Link == "" {
+		errParamIsRequired("link", "string")
+	}
+	if r.Salary <= 0 {
+		errParamIsRequired("salary", "int64")
+	}
+	return nil
+}
+
+// UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
